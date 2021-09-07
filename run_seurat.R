680fe64ae77c8f739465163bc09b70650192496d
Samuel Bunga <sambunga094@gmail.com>
2021-09-07 09:04:20 -0400

    SCTransform

--- run_seurat.R
+++ run_seurat.R
@@ -3,9 +3,13 @@ library(ggplot2)
 library(stringr)
 library(tidyseurat)
 
+## Notes
+# plot barplots of no. of cells of all samples before and after filtering
+
 # Functions
 # Visualize QC as density plots
 # nFeature
+wd <- '/Users/sbunga/gitHub/singlecell_model_analysis/'
 plot_density <- function(wd, all_data, fname, feature, intercept){
   
   png(filename = paste0(wd, 'output/images/', 'QC/',fname, '_',feature,'_density.png'), 
@@ -40,7 +44,17 @@ plot_vln <- function(wd, all_data, fname){
   dev.off()
 }
 
-wd <- '/Users/sbunga/gitHub/singlecell_model_analysis/'
+dir.create(paste0(wd, '/output/images/ncells/'), showWarnings = F)
+
+plot_ncells <- function(wd, fname, obj){
+  
+  png(paste0(wd, '/output/images/ncells/',fname, '.png'),
+      width = 1000, height = 1000, res=150)
+  print(ggplot(obj, aes(color=orig.ident, x=unname(obj@active.ident),
+                       fill= orig.ident)) + geom_bar() + xlab('') +
+    geom_text(stat='count', aes(label=..count..), vjust=-1))
+  dev.off()
+}
 
 input_dirs <- list.dirs(paste0(wd, 'input_files'), full.names = T)
 input_files <- na.omit(str_extract(input_dirs, ".*\\/[ATGC]+"))
@@ -110,8 +124,11 @@ dev.off()
 
 saveRDS(incision, paste0(wd, '/RDS/incision.Rds'))
 
+# n cells before filtering
+plot_ncells(wd,'incision_before_filter', incision)
 incision <- subset(incision, subset = nFeature_RNA > 300 & 
                      nFeature_RNA < 2500 & nCount_RNA > 500 & percent_mito < 5)
+plot_ncells(wd, 'incision_after_filter', incision)
 
 
 # Merge UVB
@@ -134,8 +151,10 @@ dev.off()
 
 saveRDS(uvb, paste0(wd, '/RDS/uvb.Rds'))
 
+plot_ncells(wd, 'uvb_before_filter', uvb)
 uvb <- subset(uvb, subset = nFeature_RNA > 250 &
                 nFeature_RNA < 2200 & nCount_RNA > 300 & percent_mito < 5)
+plot_ncells(wd, 'uvb_after_filter', uvb)
 
 # Merge Zymo
 zymo_sample <- c(9, 10, 11, 12)
@@ -156,9 +175,100 @@ VlnPlot(zymo, features = c("Thbs1","Csf1r","Ptgs2","Il1b"))
 dev.off()
 
 saveRDS(zymo, paste0(wd, '/RDS/zymo.Rds'))
+plot_ncells(wd, 'zymo_before_filter', zymo)
 zymo <- subset(zymo, subset = nFeature_RNA > 300 & 
                      nFeature_RNA < 2200 & nCount_RNA > 500 & percent_mito < 5)
+plot_ncells(wd, 'zymo_after_filter', zymo)
+
 # Merge objects
 #all_data <- merge(object_list[[1]], object_list[-1], 
 #                  add.cell.ids = sample_names )
+
+
+# Merge all the samples
+all_data <- merge(incision, c(uvb, zymo), add.cell.ids = c("incision", "uvb
+                                                           ", "zymo"))
+
+saveRDS(all_data, paste0(wd, '/RDS/merged_object.Rds'))
+
+# SCTransform
+split_seurat <- SplitObject(all_data, split.by = "orig.ident")
+
+for (i in 1:length(split_seurat)) {
+  split_seurat[[i]] <- SCTransform(split_seurat[[i]], vars.to.regress = c("percent_mito"))
+}
+
+# Select the most variable features to use for integration
+integ_features <- SelectIntegrationFeatures(object.list = split_seurat, 
+                                            nfeatures = 3000)
+
+# Prepare the SCT list object for integration
+split_seurat <- PrepSCTIntegration(object.list = split_seurat, 
+                                   anchor.features = integ_features)
+
+integ_anchors <- FindIntegrationAnchors(object.list = split_seurat, 
+                                        normalization.method = "SCT", 
+                                        anchor.features = integ_features)
+saveRDS(integ_anchors, 
+        paste0(wd, '/output/integrated_anchors.Rds'))
+
+seurat_integrated <- readRDS(paste0(wd, './output/seurat_integrated.Rds'))
+
+# Integrate across conditions
+#seurat_integrated <- IntegrateData(anchorset = integ_anchors, 
+#                                   normalization.method = "SCT")
+
+# Run PCA
+seurat_integrated <- RunPCA(object = seurat_integrated)
+
+# Plot PCA
+PCAPlot(seurat_integrated,
+        split.by = "orig.ident")
+
+# Run UMAP
+seurat_integrated <- RunUMAP(seurat_integrated, 
+                             dims = 1:40,
+                             reduction = "pca")
+
+
+# Run UMAP
+seurat_integrated <- RunUMAP(seurat_integrated, 
+                             dims = 1:40,
+                             reduction = "pca")
+
+# Explore heatmap of PCs
+DimHeatmap(seurat_integrated, 
+           dims = 1:9, 
+           cells = 500, 
+           balanced = TRUE)
+
+# Plot the elbow plot
+ElbowPlot(object = seurat_integrated, 
+          ndims = 50)
+
+# Determine the K-nearest neighbor graph
+seurat_integrated <- FindNeighbors(object = seurat_integrated, 
+                                   dims = 1:40)
+
+# Determine the clusters for various resolutions                                
+seurat_integrated <- FindClusters(object = seurat_integrated,
+                                  resolution = c(0.4, 0.6, 0.8, 1.0, 1.4))
+
+# Explore resolutions
+seurat_integrated@meta.data %>% 
+  View()
+
+# Assign identity of clusters
+Idents(object = seurat_integrated) <- "integrated_snn_res.0.6"
+
+# Plot the UMAP
+dir.create(paste0(wd, '/output/images/UMAP'), showWarnings = F)
+png(paste0(wd, '/output/images/UMAP/sample_type.png'), width = 6500,
+    height = 2500, res=400)
+print(DimPlot(seurat_integrated,
+        reduction = "umap",
+        label = TRUE,
+        label.size = 4,#group.by = 'type', 
+        split.by = 'orig.ident'))
+dev.off()
 
